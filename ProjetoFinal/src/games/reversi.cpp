413047709dc5d15e35d28f039cda0b035f23089f
João <jalvesmartins16@gmail.com>
2025-01-15 10:02:37 -0300

    fix: fixed the verifyDiagonal2Down function.

--- ProjetoFinal/src/games/reversi.cpp
+++ ProjetoFinal/src/games/reversi.cpp
@@ -152,3 +152,23 @@ bool Reversi::verifyDiagonal2Up (std::pair<int,int> play, std::vector<std::vecto
     }
     return false;
 }
+
+bool Reversi::verifyDiagonal2Down (std::pair<int,int> play, std::vector<std::vector<char>>& board, char& playingSymbol) {
+    //Verifica se é possível sequência para a diagonal secundária para cima
+    if (play.first < 6 && play.second < 6) {
+        //Verifica se o char da diagonal para baixo é da cor diferente do da jogada
+        if (board[play.first + 1][play.second - 1] != playingSymbol) {
+            //Entra em um loop até encontrar um char da mesma cor da jogada
+            int k = play.first + 1;
+            for (int i = play.second - 1; i >= 0 && k < 8; i--) {
+                if (board[k][i] == ' ') {
+                    return false;
+                } else if (board[k][i] == playingSymbol) {
+                    return true;
+                }
+                k++;
+            }
+        }
+    }
+    return false;
+}

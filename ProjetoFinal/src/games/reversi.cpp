c166b660bbe93c771b99b0f4500cde559decd1bf
João <jalvesmartins16@gmail.com>
2025-01-15 10:03:23 -0300

    feat: added the verifyPlayAdapted function.

--- ProjetoFinal/src/games/reversi.cpp
+++ ProjetoFinal/src/games/reversi.cpp
@@ -172,3 +172,29 @@ bool Reversi::verifyDiagonal2Down (std::pair<int,int> play, std::vector<std::vec
     }
     return false;
 }
+
+bool Reversi::verifyPlayAdapted (std::pair<int,int> play, char& playingSymbol) {
+  if (play.first > 7 || play.second > 7 || play.first < 0 || play.second < 0) {
+    std::cout << "ERRO: formato incorreto" << std::endl;
+    return false;
+
+  } else if (board.getBoard()[play.first][play.second] != ' ') {
+    std::cout << "ERRO: jogada inválida, essa casa já possui uma peça" << std::endl;
+    return false;
+
+  } else if (!verifyVerticalUp(play, board.getBoard(), playingSymbol) &&
+            !verifyVerticalDown(play, board.getBoard(), playingSymbol) && 
+            !verifyHorizontalLeft(play, board.getBoard(), playingSymbol) &&
+            !verifyHorizontalRight(play, board.getBoard(), playingSymbol) &&
+            !verifyDiagonalUp(play, board.getBoard(), playingSymbol) &&
+            !verifyDiagonalDown(play, board.getBoard(), playingSymbol) && 
+            !verifyDiagonal2Up(play, board.getBoard(), playingSymbol) && 
+            !verifyDiagonal2Down(play, board.getBoard(), playingSymbol)) {
+
+    std::cout << "ERRO: jogada inválida, não é posível formar uma sequência válida" << std::endl;
+    return false;
+
+  } else {
+    return true;
+  }
+}

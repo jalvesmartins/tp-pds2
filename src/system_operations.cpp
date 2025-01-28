66ea6e2e6481b04cd397bb8970906844fea31946
guilhermeapl2 <guilhermepireslopes@gmail.com>
2025-01-28 15:47:44 -0300

    feat: Addition of the methods 'handleRemovePlayer', responsible for user interaction when removing a player, and 'handleListPlayer', responsible for user interaction when listing players.

--- src/system_operations.cpp
+++ src/system_operations.cpp
@@ -46,3 +46,56 @@ void SystemOperations::handleRegisterPlayer(Registration& registry) {
 
     std::cout << "\nJogador " << nickname << " registrado com sucesso!\n";
 }
+
+void handleRemovePlayer(Registration& registry) {
+    std::string nickname;
+    bool success = false;
+    std::cin.ignore();
+
+    while (!success) {
+        try {
+            std::cout << "\nDigite o apelido do jogador a ser removido (ou digite 'sair' para voltar ao menu): ";
+            std::getline(std::cin, nickname);
+
+            if (nickname == "sair" || nickname == "Sair" || nickname == "SAIR") {
+                std::cout << "\nVoltando ao menu principal...\n";
+                return;
+            }
+
+            if (nickname.empty()) throw std::invalid_argument("Apelido nao pode ser vazio.");
+
+            success = registry.removePlayer(nickname);
+            if (!success) {
+                throw std::runtime_error("Jogador nao encontrado. Tente novamente.");
+            }
+        } catch (const std::exception& e) {
+            std::cerr << "\nERRO: " << e.what() << "\n";
+        }
+    }
+    std::cout << "\nJogador removido com sucesso.\n";
+}
+
+void handleListPlayers(Registration& registry) {
+    std::string criterion;
+    std::cin.ignore();
+    while (true) {
+        try {
+            std::cout << "\nEscolha o criterio de ordenacao: 'A' (Apelido), 'N' (Nome) ou digite 'sair' para voltar ao menu: ";
+            std::getline(std::cin, criterion);
+
+            if (criterion == "sair" || criterion == "Sair" || criterion == "SAIR") {
+                std::cout << "\nVoltando ao menu principal...\n";
+                return;
+            }
+
+            if (criterion != "A" && criterion != "a" && criterion != "N" && criterion != "n") {
+                throw std::invalid_argument("Criterio invalido. Use 'A' ou 'N'.");
+            }
+
+            registry.listPlayers(criterion);
+            break;
+        } catch (const std::exception& e) {
+            std::cerr << "\nERRO: " << e.what() << "\n";
+        }
+    }
+}
